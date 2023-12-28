b2825ec815862470132e231fc2389200368fc896
proskovey <93362757+proskovey@users.noreply.github.com>
2023-12-28 15:52:01 +0500

    Update test.app.js

--- src/__tests__/test.app.js
+++ src/__tests__/test.app.js
@@ -1,6 +1,6 @@
 import orderByProps from '../app';
 
-test('Сортировка по массиву, алфавиту', () => {
+test('Сортировка по массиву, по алфавиту name level', () => {
   const obj = {
     name: 'мечник', health: 10, level: 2, attack: 80, defence: 40,
   };
@@ -14,6 +14,38 @@ test('Сортировка по массиву, алфавиту', () => {
   ];
   expect(order).toEqual(result);
 });
+
+test('Сортировка по массиву, по алфавиту health level', () => {
+  const obj = {
+    name: 'мечник', health: 10, level: 2, attack: 80, defence: 40,
+  };
+  const order = orderByProps(obj, ['health', 'level']);
+  const result = [
+    { key: 'health', value: 10 },
+    { key: 'level', value: 2 },
+    { key: 'attack', value: 80 },
+    { key: 'defence', value: 40 },
+    { key: 'name', value: 'мечник' },
+  ];
+  expect(order).toEqual(result);
+});
+
+
+test('Сортировка по массиву, по алфавиту defence', () => {
+  const obj = {
+    name: 'мечник', health: 10, level: 2, attack: 80, defence: 40,
+  };
+  const order = orderByProps(obj, ['defence']);
+  const result = [
+    { key: 'defence', value: 40 },
+    { key: 'attack', value: 80 },
+    { key: 'health', value: 10 },
+    { key: 'level', value: 2 },
+    { key: 'name', value: 'мечник' },
+  ];
+  expect(order).toEqual(result);
+});
+
 test('Сортировка по алфавиту', () => {
   const obj = {
     name: 'мечник', health: 10, level: 2, attack: 80, defence: 40,
