d45cf5ee1d44fc897eda5cfe5fbba556bc220f1d
proskovey <93362757+proskovey@users.noreply.github.com>
2023-12-28 15:43:23 +0500

    Update test.app.js

--- src/__tests__/test.app.js
+++ src/__tests__/test.app.js
@@ -1,46 +1,30 @@
 import orderByProps from '../app';
 
-test('empty array', () => {
+test('Сортировка по массиву, алфавиту', () => {
   const obj = {
     name: 'мечник', health: 10, level: 2, attack: 80, defence: 40,
   };
-  const result = orderByProps(obj, []);
-  const expected = [
+  const order = orderByProps(obj, ['name', 'level']);
+  const result = [
+    { key: 'name', value: 'мечник' },
+    { key: 'level', value: 2 },
     { key: 'attack', value: 80 },
     { key: 'defence', value: 40 },
     { key: 'health', value: 10 },
-    { key: 'level', value: 2 },
-    { key: 'name', value: 'мечник' },
   ];
-  expect(result).toEqual(expected);
+  expect(order).toEqual(result);
 });
-
-test('array', () => {
+test('Сортировка по алфавиту', () => {
   const obj = {
     name: 'мечник', health: 10, level: 2, attack: 80, defence: 40,
   };
-  const result = orderByProps(obj, ['name', 'level']);
-  const expected = [
-    { key: 'name', value: 'мечник' },
-    { key: 'level', value: 2 },
+  const order = orderByProps(obj, []);
+  const result = [
     { key: 'attack', value: 80 },
     { key: 'defence', value: 40 },
     { key: 'health', value: 10 },
-  ];
-  expect(result).toEqual(expected);
-});
-
-test('all props in array', () => {
-  const obj = {
-    name: 'мечник', health: 10, level: 2, attack: 80, defence: 40,
-  };
-  const result = orderByProps(obj, ['name', 'level', 'health', 'attack', 'defence']);
-  const expected = [
-    { key: 'name', value: 'мечник' },
     { key: 'level', value: 2 },
-    { key: 'health', value: 10 },
-    { key: 'attack', value: 80 },
-    { key: 'defence', value: 40 },
+    { key: 'name', value: 'мечник' },
   ];
-  expect(result).toEqual(expected);
+  expect(order).toEqual(result);
 });
